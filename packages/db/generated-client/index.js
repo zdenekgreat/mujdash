09e84b87b7700447fa59ca72427629700b8866ed
zdenekgreat <zdenek@greatidea.cz>
2026-01-22 23:33:13 +0100

    5

--- packages/db/generated-client/index.js
+++ packages/db/generated-client/index.js
@@ -140,7 +140,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../.env"
   },
   "relativePath": "../prisma",
